5616e559aa0f41aae35997353eea46c9c7fba8ab
CesarQuint <pedrocesarquintero001@gmail.com>
2022-12-10 10:15:03 -0600

    Animacion

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import {useEffect} from 'react'
 import Social from './Social'
 import Avatar from '../assets/img/port1.jpg'
 
 function Landing() {
+
+  useEffect(()=>{
+    const animLetter=()=>{
+      // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+    }
+
+    animLetter()
+
+    
+  },[])
+
   return (
     <section id='Index' className='landing'>
         <div className='img_container'>
             <img src={Avatar} alt="Imagen de perfil" />
         </div>
         <div className='name'>
-            <h1>Cesar Quintero</h1>
+          <h1 class="ml9">
+            <span class="text-wrapper">
+              <span class="letters">Cesar Quintero</span>
+            </span>
+          </h1>
         </div>
         <div className='description'>
            <p>Hi I'm a self taught Front End developer and enthusiastic in always learning something new.
